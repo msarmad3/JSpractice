4e225787cb006cbf0dd4b16e8917dc19ec409667
mudassiribrahim <mudassir.comsats2018@gmail.com>
2023-08-30 15:18:18 +0500

    changes by mudassir

--- asyncawait.js
+++ asyncawait.js
@@ -5,8 +5,21 @@ async function test(){
     //console.log("i am in the async function");
 }
 //console.log(test().value);
+// after changes
 test().then(
   function(value) {console.log(value);}
 );
 console.log("outside of the async function");
 
+async function test(){
+  await setTimeout(function(){(console.log("i am in the async function"))},2000);
+  console.log("after timeout");
+  return Promise.resolve("OK");
+  //console.log("i am in the async function");
+}
+//console.log(test().value);
+// after changes
+test().then(
+function(value) {console.log(value);}
+);
+console.log("outside of the async function");
